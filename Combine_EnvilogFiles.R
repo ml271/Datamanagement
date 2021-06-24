df1684df3c714b6e6d7971aaa0f3a1580d18211a
marvinlorff <bananenhorst@gmail.com>
2021-06-24 16:59:55 +0200

    rewritting that it uses LoggerImports::readEnvilog(), works now for Esslingen!

--- Combine_EnvilogFiles.R
+++ Combine_EnvilogFiles.R
@@ -21,67 +21,103 @@
 
 # w?hle das jahr das zusammengefasst werden soll
 vch.Year <- 2021
-
+setwd("W:/R/Datamanagement")
 
 # load functions and libraries
 comtodot <- function(x) {sub(",",".",x)} 
 countna <- function(x) {sum(is.na(x))}
 library(tidyverse)
 library(lubridate)
+source("functions/readEnvilog.R")
+source("functions/check_for_ts_gaps.R")
 
 #set directory to the Esslingen (ENVILOG) and the choosen Year
 path <- paste0("O:/PROJEKT/NIEDER/LOGGER/ESSLINGN/FVA/Esslingen_Fichte_envilog/", vch.Year)
-load("readEnvilog.R")
-#-----------------------------------------------------------------------------------
-#load all csv-file from directory
-tbl <- list.files(path = path, pattern = "*.csv")
-# tbl <- tbl[-1]
-tbl <-  tbl %>%  map_df( ~read.csv2( ., skip=1,
-                                     check.names=FALSE, na.strings = "(NOREPLY)",
-                                     colClasses = c(rep("character",2), rep("numeric", 16), rep("character", 2), rep("numeric", 3))
-                        )) 
-#check columns format
-str(tbl)
 
-#change format and comma to point in columns that were not load correctly
-tbl$'#17:?C KL:9:ID(60)' <- round(as.numeric(comtodot(tbl$'#17:?C KL:9:ID(60)')),2)
-tbl$'#18:pF KL:(do.):ID(60)' <- round(as.numeric(comtodot(tbl$'#18:pF KL:(do.):ID(60)')),2)
+#-----------------------------------------------------------------------------------
+### LOADING DATA FROM PATH
+
+#get all csv-file paths from directory
+l.paths <- list.files(path = path, pattern = "*.csv", full.names = T)
+#gather data form files, use readEnvilog to make sure Sensors have identical and consistent colomns names
+dat <- l.paths %>% map_df( ~ readEnvilog(.))
+#-----------------------------------------------------------------------------------
+
+### Check Data
+# check time and other column classes
+str(dat)
+# check data ranges and nas
+summary(dat)
+# 1. check duplicated entries
+dup_check <- sum(duplicated(dat$Datum))
+if (dup_check != 0){
+  dup_dat <- dat[duplicated(dat$Datum),]
+  #dat <- dat %>%  filter(!duplicated(dat$Datum))
+}
+
+
+# 2. check for missing data
+gaps <- check_for_ts_gaps(ts= dat$Datum, max_diff= 24*60 )
+# class(gaps_alt$Dat_diff)
+# as.difftime(gaps_alt$Dat_diff, format= "%H:%M")
+# as.numeric(gaps_alt$Dat_diff, units = "days")
+# format(gaps_alt$Dat_diff)
+
+# 3. check for right date-time format und tz
+tz(dat$Datum);  class(dat$Datum); range(dat$Datum)
+dat %>%  filter( Datum >= "2021-01-01 00:00:00 UTC")
+sum(duplicated(dat$Datum))
+# 4. check data consistency
+#table(dat$Kanäle)
+
+#--------------------------------------------------------------------------------------
+
 
 # edit data, date time format, kick dublicated entries and filter for the choosen Year
-tbl1 <- tbl %>%
+dat1 <- dat %>%
   mutate(Time = dmy_hms(tbl$Time, truncated = 1)) %>%
   arrange(Time) %>% distinct(Time, .keep_all= T) %>%
   mutate(No = as.numeric(No)) %>% 
   filter(year(Time) >= vch.Year) %>% 
   filter(year(Time) < vch.Year+1) %>%  select(-No)
 
+dat1 <- 
+  dat %>% arrange(Datum) %>%
+  distinct(Datum, .keep_all= T) %>%
+  filter(!duplicated(dat$Datum)) %>%
+  filter(year(Datum) >= vch.Year) %>% 
+  filter(year(Datum) < vch.Year+1)
 
 #test
-countna(tbl1$Time)
-range(tbl1$Time, na.rm=T)
-head(tbl1)
-str(tbl1)
-countna(tbl1$No)
-l <- unique(date(tbl1$Time))
+countna(dat1$Datum)
+range(dat1$Datum, na.rm=T)
+head(dat1)
+str(dat1)
+
+l <- unique(date(dat1$Datum))
 t <- seq.Date(from = ymd(paste0(vch.Year, "-01-01")), to = ymd(paste0(vch.Year, "-12-31")), by = 1)
 l == t # alle Tage vorhanden!
 
 
 # prepare data for export
-tbl2 <- tbl1 %>% 
-  mutate(Time = format(tbl1$Time, format = "%d.%m.%Y %H:%M")) %>% 
+dat2 <- dat1 %>% 
+  mutate(Datum = format(dat1$Datum, format = "%d.%m.%Y %H:%M")) %>% 
   distinct() %>% 
   mutate(No = seq(1:nrow(.))) %>% select(No, everything())
 
 #test
-names(tbl1)
-str(tbl2)
-head(tbl2)
-tail(tbl2)
+names(dat2)
+str(dat2)
+head(dat2)
+tail(dat2)
+
+# TODO: convert data into long format
+# TODO: load all plots/Subplots at once
 
 # Export data
 # erstelle ein zusammengefasste tabelle f?r das jeweilige jahr
-write.csv2(tbl2, file=paste0("ES_Level2FI_Envilog__", vch.Year,"_combine.csv"), quote = F, row.names = F, na = "")
+writeLines("Logger: #D3000C 'Esslingen_Fi_FVA_1' - USP_EXP2 - (CGI) Expander for GP5W - (V2.60, Mai 12 2013)", paste0("data/ES_Level2FI_Envilog__", vch.Year,"_combine.csv"))
+write.table(dat2, file=paste0("data/ES_Level2FI_Envilog__", vch.Year,"_combine.csv"), sep = ";", dec=",", col.names = TRUE, append= TRUE, quote = F, row.names = F, na = "")
 
 # Zum einlesen in die Level2 Datenbank, bitte folgendes beachten:
 # Um das Datenformat aus den Loggern bei zu behalten muss nach dem die Datei erstellt wurde 
--- functions/check_for_ts_gaps.R
+++ functions/check_for_ts_gaps.R
@@ -1,9 +1,9 @@
 check_for_ts_gaps <- function(ts, max_diff, list=F){
   # 
   # #for testing
-  # ts = df.wlz$Dat_Zeit
-  # max_diff <- 24*60
-  
+  ts = dat$Datum
+  max_diff <- 24*60
+
   #conditions
   if(class(ts)[1] != "POSIXct"){
     
@@ -31,25 +31,30 @@ check_for_ts_gaps <- function(ts, max_diff, list=F){
   # table(df.alt$Dat_diff)
   # maximale time acceptable time diff
   i_l <- df$Dat_diff >= max_diff
-  i_l[1] <- FALSE
-  #i_l[85] <- TRUE
+  i_l[1] <- TRUE
+  
   i_n <- lead(i_l)
-  df.l <- df[i_l,]
-  df.n <- df[i_n,]
-  df.luecke <- rbind(df.l, df.n) %>%  arrange(Dat_Zeit)
-  df.luecke$end <- df.luecke$Dat_diff >= max_diff
-  df.luecke$luecke <- rep(c("BEGIN", "END"), length.out= length(df.luecke$Dat_Zeit) )
-  df.luecke2 <- df.luecke[-length(df.luecke$luecke),]
-  df.luecke2$no_l <- rep(1:(length(df.luecke2$Dat_Zeit)/2), each=2)
-  df.luecke3 <- df.luecke2 %>% select(Dat_Zeit, Dat_diff, luecke, no_l)
-  # return
-  l.lck_alt <- split(df.luecke3,df.luecke3$no_l )
-  df.lck_alt <- rbindlist(l.lck_alt)
-  df.lck_alt$Dat_diff <- as.difftime(df.lck_alt$Dat_diff,units = "days" )
   
-  if(list==T){
-    return(l.lck_alt)
+  if( sum(i_n, na.rm=T) >= 1){
+      df.l <- df[i_l,]
+      df.n <- df[i_n,]
+      df.luecke <- rbind(df.l, df.n) %>%  arrange(Dat_Zeit)
+      df.luecke$end <- df.luecke$Dat_diff >= max_diff
+      df.luecke$luecke <- rep(c("BEGIN", "END"), length.out= length(df.luecke$Dat_Zeit) )
+      df.luecke2 <- df.luecke[-length(df.luecke$luecke),]
+      df.luecke2$no_l <- rep(1:(length(df.luecke2$Dat_Zeit)/2), each=2)
+      df.luecke3 <- df.luecke2 %>% select(Dat_Zeit, Dat_diff, luecke, no_l)
+          
+      # return
+      l.lck_alt <- split(df.luecke3,df.luecke3$no_l )
+      df.lck_alt <- rbindlist(l.lck_alt)
+      df.lck_alt$Dat_diff <- as.difftime(df.lck_alt$Dat_diff,units = "days" )
+      
+      if(list==T){
+        return(l.lck_alt)
+      }
+      
+      return(df.lck_alt)
   }
-  
-  return(df.lck_alt)
+  else print("Keine Lücke gefunden!")
 }
